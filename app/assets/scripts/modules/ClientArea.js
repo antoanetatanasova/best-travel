76e76ef5efcdf0576ed2b61006bcdc6be23f6ca7
Antoaneta Atanasova <antoanetatanasova@gmail.com>
2020-09-21 17:54:58 +0300

    Minor issue fixed in client-area feature

--- app/assets/scripts/modules/ClientArea.js
+++ app/assets/scripts/modules/ClientArea.js
@@ -17,7 +17,7 @@ class ClientArea {
     }
 
     sendRequest() {
-        Axios.post('https://jovial-shirley-627302.netlify.app/.netlify/functions/secret-area', {password: this.form.value}).then(response => {
+        Axios.post('https://jovial-shirley-627302.netlify.app/.netlify/functions/secret-area', {password: this.field.value}).then(response => {
             this.form.remove()
             this.contentArea.innerHTML = response.data
         }).catch(() => {
